33cf05f9c31be7aa1d019fa825d175dd86f34958
DanielRogerioRibeiro <danielrogerioribeiro.dev@gmail.com>
2023-06-27 22:14:09 -0300

    First commit, importimporting libraries and defining colors

---
+++ group.py
@@ -0,0 +1,18 @@
+# Este é um programa que gera grupo de pessoas de forma aleatória
+
+# Importando as bibliotecas
+from tkinter import *
+from tkinter import ttk, filedialog
+from tkscrolledframe import ScrolledFrame
+import csv
+import random
+
+# Definido Cores
+
+co0 = "#2e2d2b"  # Preta
+co1 = "#feffff"  # Branca   
+co2 = "#e5e5e5"  # Grey
+co3 = "#00a095"  # Verde
+co4 = "#403d3d"  # Letra
+co6 = "#003452"  # Azul
+co7 = "#ef5350"  # Vermelha
